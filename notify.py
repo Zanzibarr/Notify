a59eadf1983b067e5c92127bf54b198f1c3c78b3
RickSrick <riccardo.modolo.1@studenti.unipd.it>
2023-08-02 16:02:31 +0200

    media send functions

--- notify.py
+++ notify.py
@@ -1,10 +1,32 @@
 import requests
- 
-def send(message):
 
-    TOKEN = "your_bot_token"
-    chat_id = "your_chat_id"
 
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+def set_env(token, i_chat_id):
+    global def_url,chat_id
+    def_url = f"https://api.telegram.org/bot{token}"
+    chat_id= i_chat_id
+    
 
+def send_text(message):
+    url = def_url + f"/sendMessage?chat_id={chat_id}&text={message}"
     requests.get(url)
+
+def send_media(type, file_url):
+    type = type.lower()
+    files = {
+        type : open(file_url, "rb")
+    }
+    url = def_url + f"/send{type[0].upper()+type[1:]}?chat_id={chat_id}"
+    requests.post(url, files=files)
+
+def send_photo(file_url):
+    send_media("photo", file_url=file_url)
+
+def send_document(file_url):
+    send_media("document", file_url=file_url)
+
+def send_audio(file_url):
+    send_media("audio", file_url=file_url)
+
+def send_video(file_url):
+    send_media("video", file_url=file_url)
