643b5b7b56537992f61cdc7ef8cd8be8f6f64c80
RickSrick <riccardo.modolo.1@studenti.unipd.it>
2023-08-02 17:39:13 +0200

    add markdown text input

--- notify.py
+++ notify.py
@@ -9,24 +9,31 @@ def set_env(token, i_chat_id):
 
 def send_text(message):
     url = def_url + f"/sendMessage?chat_id={chat_id}&text={message}"
-    requests.get(url)
+    requests.get(url)  
 
-def send_media(type, file_url):
+
+#USARE '''stringhe''' come quando scrivi un readme
+def send_markdown_text(message):
+    url = def_url + f"/sendMessage?chat_id={chat_id}&text={message}&parse_mode=MarkdownV2"
+    response = requests.get(url) 
+    print(response.content) 
+
+def send_media(type, file_url, caption = " "):
     type = type.lower()
     files = {
         type : open(file_url, "rb")
     }
-    url = def_url + f"/send{type[0].upper()+type[1:]}?chat_id={chat_id}"
+    url = def_url + f"/send{type[0].upper()+type[1:]}?chat_id={chat_id}&caption={caption}"
     requests.post(url, files=files)
 
-def send_photo(file_url):
-    send_media("photo", file_url=file_url)
+def send_photo(file_url,caption = " "):
+    send_media("photo", file_url, caption)
 
-def send_document(file_url):
-    send_media("document", file_url=file_url)
+def send_document(file_url,caption = " "):
+    send_media("document", file_url, caption)
 
-def send_audio(file_url):
-    send_media("audio", file_url=file_url)
+def send_audio(file_url, caption = " "):
+    send_media("audio", file_url, caption)
 
-def send_video(file_url):
-    send_media("video", file_url=file_url)
+def send_video(file_url, caption = " "):
+    send_media("video", file_url, caption)
