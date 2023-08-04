8fc890449b5cedee991f20faf2b088ace7e1d874
Zanzibarr <matteozanella2@gmail.com>
2023-08-04 12:46:23 +0200

    Added on/off option

--- notify.py
+++ notify.py
@@ -4,16 +4,27 @@ def set_env(token, i_chat_id):
     global def_url,chat_id
     def_url = f"https://api.telegram.org/bot{token}"
     chat_id= i_chat_id
+    
+def on():
+    global send
+    send = True
+    
+def off():
+    global send
+    send = False
 
 def send_text(message):
+    if not send: return
     url = def_url + f"/sendMessage?chat_id={chat_id}&text={message}"
     requests.get(url)  
 
 def send_markdown_text(message):
+    if not send: return
     url = def_url + f"/sendMessage?chat_id={chat_id}&text={message}&parse_mode=MarkdownV2"
     requests.get(url)
 
 def send_media(type, file_url, caption = " "):
+    if not send: return
     type = type.lower()
     files = {
         type : open(file_url, "rb")
