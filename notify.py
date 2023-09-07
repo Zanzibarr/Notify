9bae3edcbb948bcabb4082052b6d22add872d47a
RickSrick <riccardo.modolo.1@studenti.unipd.it>
2023-09-07 09:10:35 +0200

    add Silent option

--- notify.py
+++ notify.py
@@ -14,33 +14,33 @@ def off():
     global send
     send = False
 
-def send_text(message):
+def send_text(message, silent):
     if not send: return
-    url = def_url + f"/sendMessage?chat_id={chat_id}&text={message}"
+    url = def_url + f'''/sendMessage?chat_id={chat_id}{"&disable_notification=true" if silent is True else ""}&text={message}'''
     requests.get(url)  
 
-def send_markdown_text(message):
+def send_markdown_text(message, silent = False):
     if not send: return
-    url = def_url + f"/sendMessage?chat_id={chat_id}&text={message}&parse_mode=MarkdownV2"
+    url = def_url + f'''/sendMessage?chat_id={chat_id}{"&disable_notification=true" if silent is True else ""}&text={message}&parse_mode=MarkdownV2'''
     requests.get(url)
 
-def send_media(type, file_url, caption = " "):
+def send_media(type, file_url, caption = "", silent = False):
     if not send: return
     type = type.lower()
     files = {
         type : open(file_url, "rb")
     }
-    url = def_url + f"/send{type[0].upper()+type[1:]}?chat_id={chat_id}&caption={caption}"
+    url = def_url + f'''/send{type[0].upper()+type[1:]}?chat_id={chat_id}{"&disable_notification=true" if silent is True else ""}&caption={caption}'''
     requests.post(url, files=files)
 
-def send_photo(file_url,caption = " "):
-    send_media("photo", file_url, caption)
+def send_photo(file_url,caption = " ", silent = False):
+    send_media("photo", file_url, caption, silent)
 
-def send_document(file_url,caption = " "):
-    send_media("document", file_url, caption)
+def send_document(file_url,caption = " ", silent = False):
+    send_media("document", file_url, caption, silent)
 
-def send_audio(file_url, caption = " "):
-    send_media("audio", file_url, caption)
+def send_audio(file_url, caption = " ", silent = False):
+    send_media("audio", file_url, caption, silent)
 
-def send_video(file_url, caption = " "):
-    send_media("video", file_url, caption)
+def send_video(file_url, caption = " ", silent = False):
+    send_media("video", file_url, caption, silent)
