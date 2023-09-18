b54dd4d4a7766d455d7e76f908cabab3cc9ee190
Matteo Zanella <zaptop@pop-os.localdomain>
2023-09-18 14:55:25 +0200

    Patch 1.5.6

--- change_log.md
+++ change_log.md
@@ -1,3 +1,6 @@
+### Version 1.5.6
+- minor changes
+
 ### Version 1.5.5
 - added force update
 - added silent messages
--- notify_app.py
+++ notify_app.py
@@ -1,6 +1,6 @@
 import subprocess, requests, notify, shlex, json, sys, os
 
-version = "notify version: 1.5.5"
+version = "notify version: 1.5.6"
 
 def main():
     
